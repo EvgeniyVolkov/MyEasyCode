31fe9d544a264e565dbc29869fc9be1f62845c12
EvgeniyVolkov <volkov-ea@mail.ru>
2016-06-24 17:27:08 +0300

    *added new files

---
+++ carshowroom.php
@@ -0,0 +1,22 @@
+<?php
+$showroom = array(
+    'brands' => array(
+        'BMW' => array(
+        	'M5' => array(
+        		2002,
+        		2015
+        	)
+        ),
+        'Toyota' => array(
+        	'Corolla' => array(
+           		2002,
+        		2015
+        	),
+        	'Camry' => array(
+           		2002,
+        		2015
+        	)
+        )
+    )
+);
+print_r($showroom);
---
+++ school.php
@@ -0,0 +1,11 @@
+<?php
+$school = array(
+	'teacher' => 'Bogdan Solomykin',
+	'student1' => 'Alex',
+	'student2' => 'Sergey',
+	'student3' => 'Evgeniy'
+	);
+print_r($school);
+echo $school['teacher'];
+echo "\r\n";
+echo $school['student2'];
