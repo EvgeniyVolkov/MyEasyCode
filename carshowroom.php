395fd9cc8336f46f03f8465a8a98737452144e31
EvgeniyVolkov <volkov-ea@mail.ru>
2016-06-26 22:38:11 +0300

    * changed this file

--- carshowroom.php
+++ carshowroom.php
@@ -3,8 +3,10 @@ $showroom = array(
     'brands' => array(
         'BMW' => array(
         	'M5' => array(
+        		'years' => array(
         		2002,
         		2015
+        		)
         	)
         ),
         'Toyota' => array(
@@ -19,4 +21,12 @@ $showroom = array(
         )
     )
 );
-print_r($showroom);
+print_r($showroom); // Вывод всего массива
+print_r($showroom['brands']['BMW']['M5']['years']); // Вывод годов выпуска БМВ
+echo $showroom['brands']['Toyota']['Corolla'][0]."\r\n"; // Вывод годов выпуска Тойоты Короллы
+echo $showroom['brands']['Toyota']['Corolla'][1]."\r\n"; // Вывод годов выпуска Тойоты Короллы
+echo $showroom['brands']['Toyota']['Camry'][0]."\r\n"; // Вывод годов выпуска Тойоты Кэмри
+echo $showroom['brands']['Toyota']['Camry'][1]."\r\n"; // Вывод годов выпуска Тойоты Кэмри
+print_r($showroom['brands']['BMW']); // Вывод моделей БМВ
+print_r($showroom['brands']['Toyota']); // Вывод моделей Тойоты
+print_r($showroom['brands']); // Вывод марок авто
