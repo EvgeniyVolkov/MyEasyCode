49caa832199e87f3d3099ecfa069e46a86cd658a
EvgeniyVolkov <volkov-ea@mail.ru>
2016-06-29 20:37:49 +0300

    * changed

--- cycles-classwork.php
+++ cycles-classwork.php
@@ -1,5 +1,6 @@
 <?php
-$carBrands = array('BMW', 'Audi', 'Mercedes', 'Volvo', 'Lexus');
-for ($i=0; $i<5; $i++){
+$carBrands = array('BMW', 'Audi', 'Mercedes', 'Volvo', 'Lexus', 'Tesla', 'Porsche');
+$arrayCount = count($carBrands);
+for($i=$arrayCount-1; $i>=0; $i--){
 	echo $carBrands[$i] . ', ';
 };
