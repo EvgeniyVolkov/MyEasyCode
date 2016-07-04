477f6d872e80e3a5f594e8f0304b33eca09b0dd6
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-04 21:29:09 +0300

    *

--- classwork7.php
+++ classwork7.php
@@ -2,8 +2,14 @@
 
 // создать массив с сотней элементов от 1 до 100
 
-$numbers = array();
+/* $numbers = array();
 for ($i = 1; $i <= 100; $i++) {
+	$numbers[] = $i;
+}
+print_r($numbers); */
+
+$numbers = array();
+for ($i = 100; $i >= 1; $i--) {
 	$numbers[] = $i;
 }
 print_r($numbers);
