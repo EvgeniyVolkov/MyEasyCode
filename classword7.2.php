e06a39d03a82b1022801e8084eae7697a52c8ea3
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-04 21:51:00 +0300

    * added new file

---
+++ classword7.2.php
@@ -0,0 +1,21 @@
+<?php
+
+$colors = array();
+// ... some code
+$colors[] = 'green';
+$colors[] = 'white';
+$colors[] = 'yellow';
+echo 'Old colors array: ';
+echo '<pre>';
+print_r($colors);
+echo '</pre>';
+
+
+echo 'New colors array: ';
+$newColors = array();
+foreach ($colors as $index => $colorName) {
+	$newColors[$colorName] = $index;
+}
+echo '<pre>';
+print_r($newColors);
+echo '</pre>';
