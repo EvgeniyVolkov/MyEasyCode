4232a6b293af6e380b1333687d130b2a2a701eee
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-05 21:09:28 +0300

    * changed

--- classwork_random.php
+++ classwork_random.php
@@ -1,6 +1,12 @@
 <?php
-$random = array();
+/* $random = array();
 for ($i = 0; $i <= 100; $i++) {
 	$random[] = rand($i, true);
+}
+print_r($random); */
+$i = 0;
+while ($i <= 100) {
+	$random[] = rand(0, 100);
+	$i++;
 }
 print_r($random);
