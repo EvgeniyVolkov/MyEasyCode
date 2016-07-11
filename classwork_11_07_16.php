32104133111980fb392429a22ed4fe6af72b28e9
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-11 22:09:29 +0300

    * updated

--- classwork_11_07_16.php
+++ classwork_11_07_16.php
@@ -22,52 +22,16 @@ for ($i = 1; $i <= 100; $i++) {
 
 $randArr = array();
 $i = 1;
-while ($i <= 100) {
-    $randArr[] = rand(1, 100);
-    $i++;
-}
-/*
-$randArr2 = array();
-$i = 1;
-while ($i <= 100) {
-    $randArr2[] = rand(1, 100);
-    $i++;
-}
-*/
-// echo '<pre>';
-// print_r($randArr);
-// echo '</pre>';
-
-// echo '<pre>';
-// print_r($randArr2);
-// echo '</pre>';
-
-/*$a = 35;
-if (in_array($a, $randArr)) {
-	if (!in_array($a, $randArr2))
-		echo 'Всё ОК!';
-}*/
-/*
-$countRandArr = count($randArr);
-foreach ($randArr as $element) {
-	if (in_array($element, $randArr2)) {
-		echo $element . '<br />';
-	}
-}
-*/
-
-$randArr = array();
-$i = 1;
-while ($i <= 100) {
-    $randArr[] = rand(1, 100);
+while ($i <= 20) {
+    $randArr[] = rand(1, 20);
     $i++;
 }
 
 $randArr2 = array();
-foreach ($randArr as $element) {
-	if (in_array($element, $randArr2)) {
-		$randArr2[] = $element;
-	}
+$i = 1;
+while ($i <= 20) {
+    $randArr2[] = rand(1, 20);
+    $i++;
 }
 
 echo '<pre>';
@@ -77,3 +41,15 @@ echo '</pre>';
 echo '<pre>';
 print_r($randArr2);
 echo '</pre>';
+
+/*$a = 35;
+if (in_array($a, $randArr)) {
+	if (!in_array($a, $randArr2))
+		echo 'Всё ОК!';
+}*/
+
+foreach ($randArr as $element) {
+	if (in_array($element, $randArr2)) {
+		echo $element . '<br />';
+	}
+}
