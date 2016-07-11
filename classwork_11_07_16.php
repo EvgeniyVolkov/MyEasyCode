4728dc1afe0efd50148d5cac3cf599846f3fbb48
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-11 20:20:50 +0300

    updated

--- classwork_11_07_16.php
+++ classwork_11_07_16.php
@@ -4,7 +4,31 @@ for ($i = 1; $i <= 100; $i++) {
 	$numbers[] = $i;
 }
 // print_r($numbers);
-for ($n = 4; $n < 100; $n++) {
+/*for ($n = 4; $n < 100; $n++) {
 	echo $numbers[$n] . '<br />';
 	$n += 4;
+}*/
+/*foreach ($numbers as $number) {
+	if ($number == 10 || $number == 25) {
+		continue;
+	}
+	elseif ($number == 77) {
+		break;
+	}
+	else {
+		echo $number . '<br />';
+	}
+}*/
+
+$randArr = array();
+$i = 1;
+while ($i <= 100) {
+    $randArr[] = rand(1, 100);
+    $i++;
+}
+// echo '<pre>';
+// print_r($randArr);
+// echo '</pre>';
+if (in_array(35, $randArr)) {
+	echo 35;
 }
