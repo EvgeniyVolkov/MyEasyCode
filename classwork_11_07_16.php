20e459ac09926b4c9ae5e5cbf468fd1ece806f63
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-11 19:37:08 +0300

    added new file

---
+++ classwork_11_07_16.php
@@ -0,0 +1,10 @@
+﻿<?php
+$numbers = array();
+for ($i = 1; $i <= 100; $i++) {
+	$numbers[] = $i;
+}
+// print_r($numbers);
+for ($n = 4; $n < 100; $n++) {
+	echo $numbers[$n] . '<br />';
+	$n += 4;
+}
