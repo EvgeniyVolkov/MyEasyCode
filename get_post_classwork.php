b7116db5774c054ea4cd4831635fb0f7785279a6
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-27 21:31:37 +0300

    upd

--- get_post_classwork.php
+++ get_post_classwork.php
@@ -82,14 +82,20 @@ while ($i < $n) {
 Курс для каждой из валют задается в массиве в php  файле -->
 
 <form action="" name="test-form" method="GET">
-    <!-- Ковертируем из: <br /> -->
-    Ковертируем в: <br />
-    <select name="valuta">
+    Ковертируем из: <br />
+    <select name="valuta_from">
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="UAH">UAH</option>
-    </select>
-    <input type="text" name="sum" /> <br />
+    </select><br />
+    Ковертируем в: <br />
+    <select name="valuta_to">
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="UAH">UAH</option>
+    </select><br />
+    Сумма:<br />
+    <input type="text" name="sum" /> <br /><br />
     <input type="submit" value="Convert!">
 </form>
 
@@ -97,13 +103,24 @@ while ($i < $n) {
 
 $courses = ['USD' => 25, 'EUR' => 28, 'UAH' => 1];
 
-$valuta = $_GET['valuta'];
+$valuta_from = $_GET['valuta_from'];
+$valuta_to = $_GET['valuta_to'];
 $sum = $_GET['sum'];
 
-foreach ($courses as $key => $value) {
-	if ($valuta == $key) {
-		echo 'Результат: ' . $sum * $value;
+if ($valuta_from != $valuta_to) {
+	foreach ($courses as $valuta1 => $course1) {
+		if ($valuta_from == $valuta1) {
+			foreach ($courses as $valuta2 => $course2) {
+				if ($valuta_to == $valuta2) {
+					$result = $sum * $course1 / $course2;
+					echo $result;
+				}
+			}
+		}
 	}
+
+} else {
+	echo "Выберите валюту для конвертации!";
 }
 
 ?>
