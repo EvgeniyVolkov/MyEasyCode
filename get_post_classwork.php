0253ca99ffb8df1ac5a1c4fccf6084c673465091
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-27 20:10:51 +0300

    up

--- get_post_classwork.php
+++ get_post_classwork.php
@@ -1,4 +1,4 @@
-<!-- 1. Создать форму с такими полями:
+﻿<!-- 1. Создать форму с такими полями:
 Имя
 Фамилия
 Язык программирования, который учу
@@ -23,18 +23,49 @@
 // Меня зовут " . $_POST['name'] . " " . $_POST['lastname'] . ". " . "Я изучаю " . $_POST['lang1'] . ". Хотел бы изучать " . $_POST['lang2'] . ".";
 ?>
 
+<!-- 
 <form action="" name="test-form" method="GET">
     Слово: <input type="text" name="word" /> <br />
     Кол-во повторений: <input type="text" name="repeat" /> <br />
     <input type="submit" value="Start!">
 </form>
+ -->
 
 <?php
-
+/*
 $n = $_GET['repeat'];
 $i = 0;
 while ($i < $n) {
 	echo $_GET['word'] . '<br />';
+	$i++;
+}
+*/
+?>
+
+<form action="" name="test-form" method="POST">
+    Слово: <input type="text" name="word" /> <br />
+    Кол-во повторений: 
+    <select name="repeat">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="9">10</option>
+    </select>
+    <input type="submit" value="Start!">
+</form>
+
+<?php
+
+$n = $_POST['repeat'];
+$i = 0;
+while ($i < $n) {
+	echo strrev($_POST['word']) . '<br />';
 	$i++;
 }
 
