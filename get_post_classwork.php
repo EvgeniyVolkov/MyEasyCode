290996354eb9194ea89d841248cd8d9f1d4b658c
EvgeniyVolkov <volkov-ea@mail.ru>
2016-07-27 19:51:13 +0300

    upd

--- get_post_classwork.php
+++ get_post_classwork.php
@@ -4,6 +4,7 @@
 Язык программирования, который учу
 Язык, который хотел бы выучить -->
 
+<!--
 <form action="" name="test-form" method="POST">
     Имя: <input type="text" name="name" /> <br />
     Фамилия: <input type="text" name="lastname" /> <br />
@@ -11,12 +12,30 @@
     Язык, который хотел бы выучить: <input type="text" name="lang2" /> <br />
     <input type="submit" value="Жми!">
 </form>
+-->
 
 <!-- 2.  Принять данные и вывести следующий текст на страницу:
 Привет!
 Меня зовут <Имя> <Фамилия>. Я изучаю <Язык программирования>. Хотел бы изучать <Язык программирования>. -->
 
 <?php
-echo "Привет!<br />
-Меня зовут " . $_POST['name'] . " " . $_POST['lastname'] . ". " . "Я изучаю " . $_POST['lang1'] . ". Хотел бы изучать " . $_POST['lang2'] . ".";
+// echo "Привет!<br />
+// Меня зовут " . $_POST['name'] . " " . $_POST['lastname'] . ". " . "Я изучаю " . $_POST['lang1'] . ". Хотел бы изучать " . $_POST['lang2'] . ".";
+?>
+
+<form action="" name="test-form" method="GET">
+    Слово: <input type="text" name="word" /> <br />
+    Кол-во повторений: <input type="text" name="repeat" /> <br />
+    <input type="submit" value="Start!">
+</form>
+
+<?php
+
+$n = $_GET['repeat'];
+$i = 0;
+while ($i < $n) {
+	echo $_GET['word'] . '<br />';
+	$i++;
+}
+
 ?>
