815bed8533361a8d17300d01d82024467f263278
EvgeniyVolkov <volkov-ea@mail.ru>
2016-08-02 21:03:35 +0300

    *

--- cookies/index.php
+++ cookies/index.php
@@ -1,4 +1,4 @@
-<form action="" name="test-form" method="POST">
+<form action="index2.php" name="test-form" method="POST">
     Имя: <input type="text" name="name" /> <br /><br />
     Фамилия: <input type="text" name="lastname" /> <br /><br />
     <input type="submit">
