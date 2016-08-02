7f8eac5b3f10b9a5dce0f112c963bcc701a0b869
EvgeniyVolkov <volkov-ea@mail.ru>
2016-08-02 19:14:58 +0300

    up

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-﻿<?php
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$a = '';
+if (isset($a)) {
+	echo '$a не пустая';
+}
+
+// echo ((true ? 'true' : false) ? 't' : 'f');
