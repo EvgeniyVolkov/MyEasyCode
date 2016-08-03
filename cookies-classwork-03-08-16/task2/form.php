50101bc17809aea52a92d2f19b31f608a6af3b7c
EvgeniyVolkov <volkov-ea@mail.ru>
2016-08-03 19:57:11 +0300

    * up

--- cookies-classwork-03-08-16/task2/form.php
+++ cookies-classwork-03-08-16/task2/form.php
@@ -1,11 +1,22 @@
-<?php if (isset($_COOKIE['login'])): ?>
-    Привет, <?= $_COOKIE['login'] ?>!
-    <br />
-    Дата последнего логина: <?= $_COOKIE['lastLoginTime'] ?>
+<?php
+$login = $_GET['login'];
+$date = date('Y-m-d H:i:s');
+setcookie('lastVisitTime', $date, $time);
+if (isset($_COOKIE['login'])): ?>
+Привет, <?= $_COOKIE['login'] ?>!
+<br />
+Дата последнего логина: <?= $_COOKIE['lastLoginTime'] ?>
+<br />
+
 <?php else: ?>
     <form action="cookie_handler.php">
         Login: <input type="text" name="login" /><br />
         Cookie's lifetime: <input type="text" name="cookiesLifeTime" />
         <input type="submit" value="Send" />
     </form>
+
+    Дата последнего посещения: <?= $_COOKIE['lastVisitTime'] ?>
+
 <?php endif; ?>
+
+
