c894cea8dc408e59aa441030d22b92fd17defca0
EvgeniyVolkov <volkov-ea@mail.ru>
2016-08-03 21:00:47 +0300

    * upd

--- cookies-classwork-03-08-16/task4/index.php
+++ cookies-classwork-03-08-16/task4/index.php
@@ -1,7 +1,3 @@
-<?php
-$time = time() + 60 * 60 * 24;
-setcookie('name', $_GET['name'], $time);
-?>
 <!-- 4. Сделайте две страницы: index.php и hello.php. При заходе на index.php спросите с помощью формы имя пользователя, запишите его в куки. При заходе на hello.php поприветствуйте пользователя фразой "Привет, %Имя%!". -->
 <form action="hello.php" name="loginform" method="GET">
     Введите Ваше имя: <input type="text" name="name" /><br />
