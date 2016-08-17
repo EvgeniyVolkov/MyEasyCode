3f4b3bb76a8fa21a948944552ccf5be64b31fc35
EvgeniyVolkov <volkov-ea@mail.ru>
2016-08-17 20:03:51 +0300

    * upd

---
+++ 17.08/eugen/hideimage.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'cookie.php';
+
+if (isset($_GET['noteName'])) {
+    hideNoteImage($_GET['noteName']);
+}
+header('location: index.php');
+?>
--- 17.08/eugen/index.php
+++ 17.08/eugen/index.php
@@ -4,11 +4,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require 'cookie.php';
 ?>
-<?php
-if (isset($_GET['noteName'])) {
-    hideNoteImage($_GET['noteName']);
-}
-?>
     <form action="save.php">
         Заголовок: <input type="text" name="header"> <br />
         Текст: <textarea name="text"></textarea><br />
@@ -23,7 +18,7 @@ foreach (getData() as $key => $data):
         if (showImage($key)) {
             ?>
             <img width="300px" height="300px" src="https://habrastorage.org/files/bbb/6d0/ce5/bbb6d0ce5b2e409aa9f8fe986d87eeec.png" alt="">
-            <form>
+            <form action="hideimage.php">
                 <input type="hidden" name="noteName" value="<?= $key ?>" />
                 <input type="submit" value="Do not show me this image again">
             </form>
