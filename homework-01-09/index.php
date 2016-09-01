c3570c62968fa7d76672b4ea6be763ef9093fcda
EvgeniyVolkov <volkov-ea@mail.ru>
2016-09-01 18:37:40 +0300

    * added homework

---
+++ homework-01-09/index.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'connection.php'; // подключаем скрипт
+ 
+// подключаемся к серверу
+$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+// выполняем операции с базой данных
+$query = "SELECT name FROM author;";
+$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+if($result)
+{
+    echo "Выполнение запроса прошло успешно . <br />";
+    print_r(array get_object_vars (object $result));
+}
+     
+// закрываем подключение
+mysqli_close($link);
+?>
