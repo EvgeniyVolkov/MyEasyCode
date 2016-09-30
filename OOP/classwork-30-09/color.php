b2cf406950c4b6e509a65ba7d46c5027280d4b4a
EvgeniyVolkov <volkov-ea@mail.ru>
2016-09-30 20:19:30 +0300

    * added classwork

---
+++ OOP/classwork-30-09/color.php
@@ -0,0 +1,17 @@
+<?php
+
+class Color
+{
+    public $red;
+
+    public $green;
+
+    public $blue;
+
+    public function __construct($red, $green, $blue)
+    {
+        $this->red = $red;
+        $this->green = $green;
+        $this->blue = $blue;
+    }
+}
---
+++ OOP/classwork-30-09/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'color.php';
+
+$color = new Color(127,0,0);
+echo $color->red;
+//print_r($color);
